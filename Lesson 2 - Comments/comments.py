cd5614ecdb578ddc86cfdca366548a9cab636fd7
Anthony Beckford <abeckford532@gmail.com>
2022-08-19 15:21:38 -0400

    Add files via upload
    Learned about comments in Python

---
+++ Lesson 2 - Comments/comments.py
@@ -0,0 +1,13 @@
+# This is my first program
+
+print("This is my first comment on line 1")
+
+
+# Here is a multiple line comment
+
+'''
+This program
+is awesome
+I am making
+multiple line comments
+'''
