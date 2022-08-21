fa55e9454f928abde51ebbde152f9e8143537c9e
Anthony Beckford <abeckford532@gmail.com>
2022-08-21 15:22:17 -0400

    Add files via upload

---
+++ Lesson 5 - Strings/strings.py
@@ -0,0 +1,24 @@
+# String Formatting
+
+print("Hello Anthony")
+
+print("Hello {0} {1} {2}".format("Anthony", 100, False))
+
+
+# Followers _ | Following _ | Likes _
+followers = 100
+following = 10
+likes = "1M"
+print("Followers {0} | Following {1} | Likes {2}".format(followers, following, likes))
+
+
+# String Manipulation (Updating a String value)
+myName = "Beth +"
+myName += " Anthony <3"
+print(myName)
+
+
+
+x = "x"
+print(x)
+print(x * 10)
