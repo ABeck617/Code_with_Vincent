1cf68ee33f25c361d80afba8a52d0c06091ec7c1
Anthony Beckford <abeckford532@gmail.com>
2022-08-21 14:21:23 -0400

    Add files via upload

---
+++ Lesson 4 - Updating Variables/variables.py
@@ -0,0 +1,13 @@
+# In python you can change the value with the same variable name but the order matters
+
+myName = "Anthony"
+print(myName)
+myName = "David"
+print(myName)
+
+
+lessonNumber = 4
+print(lessonNumber)
+lessonNumber = 5
+print(lessonNumber)
+lessonNumber += 1
