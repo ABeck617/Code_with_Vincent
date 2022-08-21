16d56d56b0f2b523b78602ea77b430637ec8fee5
Anthony Beckford <abeckford532@gmail.com>
2022-08-21 15:45:17 -0400

    Add files via upload

---
+++ Lesson 6 - Functions/functions.py
@@ -0,0 +1,12 @@
+print("Step 1: Mix the Flower")
+print("Step 2: Bake the Cake")
+print("Step 3: Put icing on the cake")
+
+# Defining the function
+def cakeRecipe():
+   print("Step 1: Mix the Flower")
+   print("Step 2: Bake the Cake")
+   print("Step 3: Put icing on the cake") 
+
+# Calling the function
+cakeRecipe()
