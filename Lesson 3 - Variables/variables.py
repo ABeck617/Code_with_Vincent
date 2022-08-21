6d029af1b573c4ec2ca1a2f9852a547af7e85a6e
Anthony Beckford <abeckford532@gmail.com>
2022-08-21 09:38:24 -0400

    Add files via upload
    Learned about variables

---
+++ Lesson 3 - Variables/variables.py
@@ -0,0 +1,11 @@
+# Create a variable called myName and store the string value called "Anthony"
+
+myName = "Anthony"
+
+# Print out the variable myName
+
+print(myName)
+
+# Print the type of variable
+
+print(type(myName))
