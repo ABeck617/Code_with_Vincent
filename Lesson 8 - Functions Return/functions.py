ab6700e880f36348dc7fc6d384ed8cc0fac2d8e8
Anthony Beckford <abeckford532@gmail.com>
2022-10-14 23:33:19 -0400

    Add files via upload

---
+++ Lesson 8 - Functions Return/functions.py
@@ -0,0 +1,16 @@
+def double(x):
+    return x * 2
+
+
+print(double(99))
+
+name = "Anthony"
+print("Hello Everyone, My name is {0}".format(name))
+
+
+def f(x):
+    return 5 * x - 3
+
+print("I will call function f(x) with 5")
+print(f(5))
+print("Done")
